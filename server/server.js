92fdc2371ee866399cc651a122cbcbf19619aaf1
JavisNguyen <dai12051997@gmail.com>
2018-09-08 08:56:31 +0700

    no message

--- server/server.js
+++ server/server.js
@@ -2,9 +2,7 @@
 
 var loopback = require('loopback');
 var boot = require('loopback-boot');
-
 var app = module.exports = loopback();
-
 app.start = function() {
   // start the web server
   return app.listen(function() {
@@ -17,7 +15,6 @@ app.start = function() {
     }
   });
 };
-
 // Bootstrap the application, configure models, datasources and middleware.
 // Sub-apps like REST API are mounted via boot scripts.
 boot(app, __dirname, function(err) {
@@ -25,5 +22,5 @@ boot(app, __dirname, function(err) {
 
   // start the server if `$ node server.js`
   if (require.main === module)
-    app.start();
+  app.start();
 });
